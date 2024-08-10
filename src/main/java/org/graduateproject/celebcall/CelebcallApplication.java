d6ce0e3fbcc7dc751d4430e7a924f06e8716ae91
정태승 <hi563@g.skku.edu>
2024-08-10 17:37:29 +0900

    init: 프로젝트 설정

---
+++ src/main/java/org/graduateproject/celebcall/CelebcallApplication.java
@@ -0,0 +1,13 @@
+package org.graduateproject.celebcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+
+@SpringBootApplication
+public class CelebcallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CelebcallApplication.class, args);
+    }
+}
---
+++ src/test/java/org/graduateproject/celebcall/CelebcallApplicationTests.java
@@ -0,0 +1,13 @@
+package org.graduateproject.celebcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelebcallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
